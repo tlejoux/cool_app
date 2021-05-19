16e77c2d4f1b3f3e842b6bf3c4f4c037558f7fc5
tlejoux <thibault.lejoux@epfedu.fr>
2021-05-19 20:20:33 +0200

    third commit

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
 import streamlit as st
 st.title("Hello world")
+
+import os
+import re
+
+code = """<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXXX-4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-XXXXXXXXX-4');
+</script>"""
+
+a=os.path.dirname(st.__file__) + '/static/index.html'
+with open(a, 'r') as f:
+    data=f.read()
+    if len(re.findall('UA-', data)) == 0:
+        with open(a, 'w') as f:
+            newdata=re.sub('<head>','<head>' + code, data)
+            f.write(newdata)
