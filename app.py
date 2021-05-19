ca2031bc39701e788dd668d9a598ecc5d3853700
tlejoux <thibault.lejoux@epfedu.fr>
2021-05-19 22:13:51 +0200

    app.py update

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ import os
 import re
 import streamlit as st
 
+
 code = """<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-197493543-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'UA-197493543-1);
+
+  gtag('config', 'UA-197493543-1');
 </script>"""
+
 
 a=os.path.dirname(st.__file__) + '/static/index.html'
 with open(a, 'r') as f:
@@ -18,8 +21,6 @@ with open(a, 'r') as f:
         with open(a, 'w') as f:
             newdata=re.sub('<head>','<head>' + code, data)
             f.write(newdata)
-            
-
 
 st.title("Hello world")
 
